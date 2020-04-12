a2e3059b4833ace4cab50c28364711c217a7146b
Zisso <aviv.zisso.az@gmail.com>
2020-04-12 13:08:28 +0300

    add header

---
+++ wsl-samples/afunix-wsl-interop/linux/header.h
@@ -0,0 +1,8 @@
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define SERVER_SOCKET "server.sock"
+#define CLIENT_SOCKET "client.sock"
