c0887c955ea782b390b3b60a6b53844c5a482416
l33ds <railway.and.gun@gmail.com>
2020-05-28 22:06:56 +0300

    Refactor wsl examples (#624)
    * Fix compilation issues including C4996 and C4477
    
    * fix warning and ignore external warnings
    
    Co-authored-by: Zisso <aviv.zisso.az@gmail.com>

--- wsl-samples/afunix-wsl-interop/linux/client.c
+++ wsl-samples/afunix-wsl-interop/linux/client.c
@@ -1,71 +1,59 @@
-#include <sys/socket.h>
-#include <sys/un.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+#include "header.h"
 
-char *server_full_path = "server.sock";
-char *client_full_path = "client.sock";
+int main(int argc, char* argv[]) {
+	char Buf[100] = { 0 };
+	ssize_t BytesRecvd = 0;
+	struct sockaddr_un clientAddr = { 0 };
+	struct sockaddr_un serverAddr = { 0 };
+	struct sockaddr_un address = { 0 };
+	char serverAddress[100] = { 0 };
+	int serverFd = 0, rc = 0, clientFd = 0;
+	int acceptFd = 0;
+	socklen_t addrLen = 0;
 
-int main(int argc, char *argv[]) {
-  char Buf[100];
-  ssize_t BytesRecvd;
-  struct sockaddr_un clientAddr = {0};
-  struct sockaddr_un serverAddr = {0};
-  struct sockaddr_un address;
-  char serverAddress[100] = {0};
-  int serverFd, rc, clientFd;
-  int acceptFd;
-  socklen_t addrLen;
+	if ((clientFd = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) {
+		perror("clientFd socket error");
+		goto ErrorExit;
+	}
 
-  if ((clientFd = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) {
-    perror("clientFd socket error");
-    goto ErrorExit;
-  }
+	clientAddr.sun_family = AF_UNIX;
+	strncpy(clientAddr.sun_path, CLIENT_SOCKET, sizeof(clientAddr.sun_path) - 1);
+	if (bind(clientFd, (struct sockaddr*)&clientAddr, sizeof(clientAddr)) == -1) {
+		perror("server bind error");
+		goto ErrorExit;
+	}
 
-  memset(&clientAddr, 0, sizeof(clientAddr));
-  clientAddr.sun_family = AF_UNIX;
-  strncpy(clientAddr.sun_path, client_full_path, sizeof(clientAddr.sun_path)-1);
-  if (bind(clientFd, (struct sockaddr*)&clientAddr, sizeof(clientAddr)) == -1) {
-    perror("server bind error");
-    goto ErrorExit;
-  }
+	addrLen = sizeof(clientAddr);
+	if (getsockname(clientFd, (struct sockaddr*)&clientAddr, &addrLen) == -1) {
+		perror("getsockname(client)");
+		goto ErrorExit;
+	}
 
-  addrLen = sizeof(clientAddr);
-  memset(&clientAddr, 0, sizeof(clientAddr));
-  if (getsockname(clientFd, (struct sockaddr*)&clientAddr, &addrLen) == -1) {
-    perror("getsockname(client)");
-    goto ErrorExit;
-  }
+	printf("getsockname returned: %s, addressize: %d\n", clientAddr.sun_path, addrLen);
+	serverAddr.sun_family = AF_UNIX;
+	strncpy(serverAddr.sun_path, SERVER_SOCKET, sizeof(serverAddr.sun_path));
+	printf("client: connecting to %s\n", SERVER_SOCKET);
+	if (connect(clientFd, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) == -1) {
+		perror("connect error");
+		goto ErrorExit;
+	}
 
-  printf("getsockname returned: %s, addressize: %d\n", clientAddr.sun_path, addrLen);
-  memset(&serverAddr, 0, sizeof(serverAddr));
-  serverAddr.sun_family = AF_UNIX;
-  strncpy(serverAddr.sun_path, server_full_path, sizeof(serverAddr.sun_path));
-  printf("client: connecting to %s\n", serverAddress);
-  if (connect(clientFd, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) == -1) {
-    perror("connect error");
-    goto ErrorExit;
-  }
+	printf("client: connected to the server\n");
+	addrLen = sizeof(clientAddr);
+	if (getpeername(clientFd, (struct sockaddr*)&clientAddr, &addrLen) == -1) {
+		perror("getpeername(client)");
+		goto ErrorExit;
+	}
 
-  printf("client: connected to the server\n");
-  addrLen = sizeof(clientAddr);
-  memset(&clientAddr, 0, sizeof(clientAddr));
-  if (getpeername(clientFd, (struct sockaddr*)&clientAddr, &addrLen) == -1) {
-    perror("getpeername(client)");
-    goto ErrorExit;
-  }
+	printf("getpeername returned: %s, addressize: %d\n", clientAddr.sun_path, addrLen);
+	if ((BytesRecvd = recv(clientFd, Buf, sizeof(Buf), 0)) == -1) {
+		perror("recv");
+		goto ErrorExit;
+	}
 
-  printf("getpeername returned: %s, addressize: %d\n", clientAddr.sun_path, addrLen);
-  memset(Buf, 0, sizeof(Buf));
-  if ((BytesRecvd = recv(clientFd, Buf, sizeof(Buf), 0)) == -1) {
-    perror("recv");
-    goto ErrorExit;
-  } 
+	printf("received: %zd bytes, %s\n", BytesRecvd, Buf);
 
-  printf("received: %zd bytes, %s\n", BytesRecvd, Buf);
- 
 ErrorExit:
-  unlink(client_full_path);
-  return 0;
+	unlink(CLIENT_SOCKET);
+	return 0;
 }
